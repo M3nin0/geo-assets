2e0718d4db995d8aadb23b4aa44f4669f73e5a4b
Felipe Menino <efelipecarlos@gmail.com>
2022-04-23 13:35:38 -0300

    package: updating code style

--- geo_assets/__init__.py
+++ geo_assets/__init__.py
@@ -5,9 +5,9 @@
 # geo-assets is free software; you can redistribute it and/or modify it under
 # the terms of the MIT License; see LICENSE file for more details.
 
-"""GEO Knowledge Hub assets"""
+"""GEO Assets module for GEO Knowledge Hub."""
 
 from .ext import GEOAssets
 from .version import __version__
 
-__all__ = ('__version__', 'GEOAssets')
+__all__ = ("__version__", "GEOAssets")
